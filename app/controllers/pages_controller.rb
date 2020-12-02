86a88ea87aca3511f56d11e24e801c05acb48612
Julien Corion <astram59@gmail.com>
2020-12-02 19:49:20 +0100

    typo & first icons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def uikit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/uikit', to: 'pages#uikit', as: 'uikit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: %i[index new create show destroy] do 
     resources :contents, only: %i[new create destroy]
