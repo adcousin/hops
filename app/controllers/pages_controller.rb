be7bf1339b3fab0cc9b91abb1665e1730a9bba67
Jean Pierre Symoens <jpsymoens0@gmail.com>
2020-12-03 11:11:32 +0100

    added .includes to optimize query performance

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @beers = Beer.order('RANDOM()').limit(6)
+    @beers = Beer.order('RANDOM()').includes(:brewery, :style, brewery: [:country]).limit(6)
   end
 end
