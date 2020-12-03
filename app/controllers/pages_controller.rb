2a0ecc04767e77bb11440ec05b0b9094b3e4897e
Jean Pierre Symoens <jpsymoens0@gmail.com>
2020-12-03 09:12:55 +0100

    Added basic infos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    Beer.order('RANDOM()').limit(6)
+    @beers = Beer.order('RANDOM()').limit(6)
   end
 end
