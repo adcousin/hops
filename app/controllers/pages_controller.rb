3879d447cff0c067d70087647d478cda3ec697d8
Jean Pierre Symoens <jpsymoens0@gmail.com>
2020-12-03 12:12:01 +0100

    Added a picture and the title without css so UGLY

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @beers = Beer.order('RANDOM()').includes(:brewery, :style, brewery: [:country]).limit(6)
+    @beers = Beer.order('RANDOM()').includes(:brewery, :style, :color, brewery: [:country]).limit(6)
   end
 end
