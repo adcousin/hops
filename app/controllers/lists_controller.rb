e5941d617b90a5d8fc77c9494d4950d5209d18b4
adrien cousin <adrien.cousin@lilo.org>
2020-12-08 10:23:59 +0100

    Implemented beer-card-lg-variable in lists#index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,6 +18,17 @@ class ListsController < ApplicationController
     @blacklist = Beer.joins(:lists).where("lists.name = 'Blacklist' AND lists.user_id = ?", current_user.id)
     @wishlist = Beer.joins(:lists).where("lists.name = 'Wishlist' AND lists.user_id = ?", current_user.id)
     @custom_lists = Beer.joins(:lists).where("lists.name NOT IN ('Whitelist', 'Blacklist', 'Wishlist') AND lists.user_id = ?", current_user.id)
+
+    # Initialize arrays to store beer_id of each core list
+    # Inclusion of beer.id in the list displays the icon or not on the card-lg
+    @blacklist_ids = []
+    @whitelist_ids = []
+    @wishlist_ids = []
+    @customlists_ids = []
+    @blacklist.each {|x| @blacklist_ids << x.id} if
+    @whitelist.each {|x| @whitelist_ids << x.id}
+    @wishlist.each {|x| @wishlist_ids << x.id}
+    @custom_lists.each {|x| @customlists_ids << x.id}
   end
 
   def new
