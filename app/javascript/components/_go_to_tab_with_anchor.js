da89342d5a6c800cf9578d231ca933e5b6c4b795
adrien cousin <adrien.cousin@lilo.org>
2020-12-09 17:51:38 +0100

    Put declaration in function to recompute at each refresh

--- app/javascript/components/_go_to_tab_with_anchor.js
+++ app/javascript/components/_go_to_tab_with_anchor.js
@@ -1,8 +1,9 @@
-const model = window.location.pathname.substring(1)
-const anchor = window.location.hash.substring(1)
-const element = document.getElementById(`${anchor}-tab`)
+
 
 const goToTabWithAnchor = () => {
+  const model = window.location.pathname.substring(1)
+  const anchor = window.location.hash.substring(1)
+  const element = document.getElementById(`${anchor}-tab`)
   if (element) {
     element.click()
   };
