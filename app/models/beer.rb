c901841924e2df298fd8c600221e3f8382821d0c
Stephane Briclot <stephane.briclot@gmail.com>
2020-12-09 10:18:30 +0100

    Fix validation rules for Beer

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -9,9 +9,9 @@ class Beer < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
 
-  validates :name, :brewery_id, :color_id, :style_id, :alcohol_strength, :ibu, presence: true
+  validates :name, :brewery_id, :color_id, :style_id, :alcohol_strength, presence: true
   validates :alcohol_strength, numericality: { :greater_than_or_equal_to => 0 }
-  validates :ibu, numericality: { only_integer: true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 150 }
+  validates :ibu, numericality: { only_integer: true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 150 }, allow_nil: true
   before_save :capitalize
 
   include PgSearch::Model
